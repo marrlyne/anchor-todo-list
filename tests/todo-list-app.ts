49217b8ff1d7ff847403bfe982b2ad0326f530ae
Marrlyne <marrlyne63@gmail.com>
2024-06-08 01:01:48 +0100

    Update todo-list-app.ts

--- tests/todo-list-app.ts
+++ tests/todo-list-app.ts
@@ -33,107 +33,107 @@ describe("todo-list-app", () => {
     assert.ok(taskAccount.updatedAt);
   });
 
-  // it("can create a new task from a different author", async () => {
-  //   const user = anchor.web3.Keypair.generate();
+  it("can create a new task from a different author", async () => {
+    const user = anchor.web3.Keypair.generate();
 
-  //   const signature = await program.provider.connection.requestAirdrop(
-  //     user.publicKey,
-  //     10000000000
-  //   );
+    const signature = await program.provider.connection.requestAirdrop(
+      user.publicKey,
+      10000000000
+    );
 
-  //   const latestBlockHash =
-  //     await program.provider.connection.getLatestBlockhash();
+    const latestBlockHash =
+      await program.provider.connection.getLatestBlockhash();
 
-  //   await program.provider.connection.confirmTransaction({
-  //     blockhash: latestBlockHash.blockhash,
-  //     lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-  //     signature,
-  //   });
+    await program.provider.connection.confirmTransaction({
+      blockhash: latestBlockHash.blockhash,
+      lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+      signature,
+    });
 
-  //   const task = anchor.web3.Keypair.generate();
+    const task = anchor.web3.Keypair.generate();
 
-  //   const tx = await program.methods
-  //     .addingTask("You are more than awesome")
-  //     .accounts({
-  //       task: task.publicKey,
-  //       author: user.publicKey,
-  //       systemProgram: anchor.web3.SystemProgram.programId,
-  //     })
-  //     .signers([task, user])
-  //     .rpc();
+    const tx = await program.methods
+      .addingTask("You are more than awesome")
+      .accounts({
+        task: task.publicKey,
+        author: user.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([task, user])
+      .rpc();
 
-  //   console.log("Your transaction signature", tx);
-  //   const taskAccount = await program.account.task.fetch(task.publicKey);
-  //   console.log("Your task", taskAccount);
+    console.log("Your transaction signature", tx);
+    const taskAccount = await program.account.task.fetch(task.publicKey);
+    console.log("Your task", taskAccount);
 
-  //   assert.equal(taskAccount.author.toBase58(), user.publicKey.toBase58());
-  //   assert.equal(taskAccount.text, "You are more than awesome");
-  //   assert.equal(taskAccount.isDone, false);
-  //   assert.ok(taskAccount.createdAt);
-  //   assert.ok(taskAccount.updatedAt);
-  // });
+    assert.equal(taskAccount.author.toBase58(), user.publicKey.toBase58());
+    assert.equal(taskAccount.text, "You are more than awesome");
+    assert.equal(taskAccount.isDone, false);
+    assert.ok(taskAccount.createdAt);
+    assert.ok(taskAccount.updatedAt);
+  });
 
-  // it("can fetch all tasks", async () => {
-  //   const tasks = await program.account.task.all();
-  //   console.log("Your tasks", tasks);
-  // });
+  it("can fetch all tasks", async () => {
+    const tasks = await program.account.task.all();
+    console.log("Your tasks", tasks);
+  });
 
-  // it("can filter tasks by author", async () => {
-  //   const authorPublicKey = author.wallet.publicKey;
-  //   console.log("authorPublicKey", authorPublicKey.toBase58());
-  //   const tasks = await program.account.task.all([
-  //     {
-  //       memcmp: {
-  //         offset: 8,
-  //         bytes: authorPublicKey.toBase58(),
-  //       },
-  //     },
-  //   ]);
+  it("can filter tasks by author", async () => {
+    const authorPublicKey = author.wallet.publicKey;
+    console.log("authorPublicKey", authorPublicKey.toBase58());
+    const tasks = await program.account.task.all([
+      {
+        memcmp: {
+          offset: 8,
+          bytes: authorPublicKey.toBase58(),
+        },
+      },
+    ]);
 
-  //   assert.equal(tasks.length, 1);
-  // });
+    assert.equal(tasks.length, 1);
+  });
 
-  // it("can update a task to done", async () => {
-  //   const task = anchor.web3.Keypair.generate();
-  //   // create a new task
-  //   // fetch it from the same address
-  //   // then update
-  //   const tx = await program.methods
-  //     .updatingTask(true)
-  //     .accounts({
-  //       task: task.publicKey,
-  //       author: author.wallet.publicKey,
-  //     })
-  //     .signers([author])
-  //     .rpc();
+  it("can update a task to done", async () => {
+    const task = anchor.web3.Keypair.generate();
+    // create a new task
+    // fetch it from the same address
+    // then update
+    const tx = await program.methods
+      .updatingTask(true)
+      .accounts({
+        task: task.publicKey,
+        author: author.wallet.publicKey,
+      })
+      .signers([author])
+      .rpc();
 
-  //   console.log("Your transaction signature", tx);
+    console.log("Your transaction signature", tx);
 
-  //   const taskAccount = await program.account.task.fetch(task.publicKey);
-  //   console.log("Your task", taskAccount);
+    const taskAccount = await program.account.task.fetch(task.publicKey);
+    console.log("Your task", taskAccount);
 
-  //   assert.equal(
-  //     taskAccount.author.toBase58(),
-  //     author.wallet.publicKey.toBase58()
-  //   );
-  //   assert.equal(taskAccount.isDone, true);
-  // });
+    assert.equal(
+      taskAccount.author.toBase58(),
+      author.wallet.publicKey.toBase58()
+    );
+    assert.equal(taskAccount.isDone, true);
+  });
 
-  // it("cannot create a task with more than 400 characters", async () => {
-  //   const task = anchor.web3.Keypair.generate();
-  //   try {
-  //     await program.methods
-  //       .addingTask("You are awesome".repeat(100))
-  //       .accounts({
-  //         task: task.publicKey,
-  //         author: author.wallet.publicKey,
-  //         systemProgram: anchor.web3.SystemProgram.programId,
-  //       })
-  //       .signers([task])
-  //       .rpc();
-  //     assert.fail("Expected an error");
-  //   } catch (err) {
-  //     assert.equal(err.toString(), "Error: failed to send transaction");
-  //   }
-  // });
+  it("cannot create a task with more than 400 characters", async () => {
+    const task = anchor.web3.Keypair.generate();
+    try {
+      await program.methods
+        .addingTask("You are awesome".repeat(100))
+        .accounts({
+          task: task.publicKey,
+          author: author.wallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .signers([task])
+        .rpc();
+      assert.fail("Expected an error");
+    } catch (err) {
+      assert.equal(err.toString(), "Error: failed to send transaction");
+    }
+  });
 });
